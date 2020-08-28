266ee7fad561f7ff869967fca3abd74882925dbd
DELL <layesall05@gmail.com>
2020-08-28 16:49:40 +0000

    Réglages

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -251,6 +251,7 @@ class User implements UserInterface
      */
     public function eraseCredentials()
     {
+        
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
